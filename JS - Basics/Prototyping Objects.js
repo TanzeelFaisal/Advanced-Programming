9f88bf21ce958e642fdc40d82b83850d6456a80d
Tanzeel Faisal <tanzeelfaisal@outlook.com>
2023-09-23 13:11:35 +0500

    Lec 6

--- JS - Basics/Prototyping Objects.js
+++ JS - Basics/Prototyping Objects.js
@@ -20,3 +20,74 @@ myTemp.prototype.display = function() {return this.anme}    // Adds method in te
 
 Object.prototype.addone = 9 // Unethical practice: Updates the built-in template, obviously not a good practice
 
+function Employee(n, s) {
+    this.name = n, 
+    this.salary = s
+}
+
+let harry = new Employee("harry", 2000)
+
+console.log(harry)
+
+Employee.prototype.describe = function() {return "this is a company"}
+
+console.log(harry.describe())
+
+// Using constructor of Employee in HR
+function HR (n, s, l) {
+    Employee.call(this, n, s),
+    this.language = l
+}
+
+let Sarah = new HR("Sarah", 3000, "English")
+
+// Sarah.describe()    // Even when constructor of Employee is called, object not attached with Employee in the chain and .describe() is not called
+
+// For this we need to add Employee as a prototype in HR
+HR.prototype = Object.create(Employee.prototype)
+
+Sarah = new HR("Sarah", 3000, "English")    // This time, prototype will be attached with Employee, however HR's prototype is dismissed and its constructor is not called
+                                            // Employee > HR > Object
+console.log(Sarah)
+
+// We'll have to manually call HR constructor
+
+HR.prototype.constructor = HR
+
+// Rather than all this mess, we can use classes
+class Emp {
+    constructor(n, s) {
+        this.name = n,
+        this.salary = s
+    }
+}
+
+class HR1 extends Emp{
+    constructor(n, s, l) {
+        super(n, s),this.language = l
+    }
+}
+
+// Creating a method in the built-in array prototype
+Array.prototype.upper = function() {
+    for (let i in this) {
+        this[i] = this[i].toUpperCase()
+    }
+    return this
+}
+
+function myArray (arr) {
+    this.arr = arr
+}
+
+myArray.prototype = Object.create(Array.prototype)
+
+let obj2 = {myprop : 2}
+obj1 = obj2
+
+// ... operator unpacks values
+
+let a = [1, 2]
+let a1 = [...a, 3, 4]
+
+[a, ,b] = ["sara", "ali", "hira"]
